b7914c3d997efd94f4937d0c83477660f5e23ce5
Andrew Kane <andrew@ankane.org>
2024-09-08 16:04:07 -0700

    Added Vector class

--- lib/pgvector.dart
+++ lib/pgvector.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'vector.dart';
+
 class Pgvector {
   const Pgvector();
 
   // encode as text
   String encode(List<double> input) {
-    return input.toString();
+    return Vector(input).toString();
   }
 
   // decode from binary
@@ -20,16 +22,7 @@ class Pgvector {
       input = Uint8List.fromList(utf8.encode(input));
     }
     var bdata = new ByteData.view(input.buffer, input.offsetInBytes);
-    var dim = bdata.getUint16(0);
-    var unused = bdata.getUint16(2);
-    if (unused != 0) {
-      throw FormatException('expected unused to be 0');
-    }
-    var vec = <double>[];
-    for (var i = 0; i < dim; i++) {
-      vec.add(bdata.getFloat32(4 + i * 4));
-    }
-    return vec;
+    return Vector.fromBinary(bdata).toList();
   }
 }
 
---
+++ lib/vector.dart
@@ -0,0 +1,29 @@
+import 'dart:typed_data';
+
+class Vector {
+  final List<double> vec;
+
+  const Vector(this.vec);
+
+  factory Vector.fromBinary(ByteData bdata) {
+    var dim = bdata.getUint16(0);
+    var unused = bdata.getUint16(2);
+    if (unused != 0) {
+      throw FormatException('expected unused to be 0');
+    }
+    var vec = <double>[];
+    for (var i = 0; i < dim; i++) {
+      vec.add(bdata.getFloat32(4 + i * 4));
+    }
+    return Vector(vec);
+  }
+
+  List<double> toList() {
+    return vec;
+  }
+
+  @override
+  String toString() {
+    return vec.toString();
+  }
+}
