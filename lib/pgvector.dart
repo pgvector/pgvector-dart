cfab8e6202c178b3baa03b7cf3db2a4b27f8ed5c
Andrew Kane <andrew@ankane.org>
2024-09-09 22:02:53 -0700

    Improved code [skip ci]

--- lib/pgvector.dart
+++ lib/pgvector.dart
@@ -1,30 +1,4 @@
-import 'dart:convert';
-import 'src/vector.dart';
-
 export 'src/halfvec.dart' show HalfVector;
+export 'src/pgvector.dart' show pgvector;
 export 'src/sparsevec.dart' show SparseVector;
 export 'src/vector.dart' show Vector;
-
-class Pgvector {
-  const Pgvector();
-
-  // encode as text
-  String encode(List<double> input) {
-    return Vector(input).toString();
-  }
-
-  // decode from binary
-  // TODO find a way to make encode/decode consistent
-  List<double> decode(dynamic input) {
-    // PostgresBinaryDecoder in the postgres package
-    // tries to decode as utf8 for unknown types
-    // sometimes it succeeds, other times it fails
-    // so need to handle both cases
-    if (input is String) {
-      input = utf8.encode(input);
-    }
-    return Vector.fromBinary(input).toList();
-  }
-}
-
-const Pgvector pgvector = Pgvector();
